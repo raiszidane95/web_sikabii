bde4303f5b99aadc6aa2c0a7750c3ebb4f7b9537
raiszidane95 <raiszidane95@gmail.com>
2022-05-22 22:04:21 +0700

    Change modal view

--- resources/views/admin/data-kegiatan.blade.php
+++ resources/views/admin/data-kegiatan.blade.php
@@ -7,24 +7,31 @@
                 <h3>Data Kegiatan</h3>
                 <div class="container mt-5 mb-2">
                     <!-- Button trigger modal -->
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <button type="button" class="btn btn-primary mb-1" data-toggle="modal" data-target="#largeModal">
                         Tambah Data
                     </button>
 
                     <!-- Modal -->
-                    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
-                        tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                    <div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-lg" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <h5 class="modal-title" id="staticBackdropLabel">Tambah Data</h5>
+                                    <h5 class="modal-title" id="largeModalLabel">Large Modal</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
                                 </div>
                                 <div class="modal-body">
-                                    ...
+                                    <p>
+                                        There are three species of zebras: the plains zebra, the mountain zebra and the Grévy's zebra. The plains zebra
+                                        and the mountain zebra belong to the subgenus Hippotigris, but Grévy's zebra is the sole species of subgenus
+                                        Dolichohippus. The latter resembles an ass, to which it is closely related, while the former two are more
+                                        horse-like. All three belong to the genus Equus, along with other living equids.
+                                    </p>
                                 </div>
                                 <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                    <button type="button" class="btn btn-primary">Understood</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                                    <button type="button" class="btn btn-primary">Confirm</button>
                                 </div>
                             </div>
                         </div>
