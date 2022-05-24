11f88e67cb3201c311d32616bc2f0490c0d29ed6
raiszidane95 <raiszidane95@gmail.com>
2022-05-24 13:40:02 +0700

    Add view detail data kepengurusan

---
+++ resources/views/admin/detail-data-kepengurusan.blade.php
@@ -0,0 +1,19 @@
+@extends('admin.dashboard')
+@section('content')
+
+    <body>
+        <div class="container ">
+            <div class="col-lg-10">
+                <h3>Detail Kepengurusan</h3>
+            <div class="col">
+                <p>
+                </p>
+            </div>
+                <div class="container mt-5 mb-2">
+                </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+                        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+            </script>
+    </body>
+@endsection
