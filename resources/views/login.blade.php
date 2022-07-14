b8edfd873260c1b92511dccec1616ee249012eca
raiszidane95 <raiszidane95@gmail.com>
2022-07-15 05:15:12 +0700

    Fix responsive view Register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,42 +5,46 @@
         <h3>Login Akun</h3>
         <h5>Kaderisasi Birohmah Unila</h5>
     </div>
-
     <div class="container mt-5">
         <div class="row row-cols-auto">
             <div class=" container col-auto">
-                @if (session()->has('success'))
-                    <div class="alert alert-success alert-dismissible fade show" role="alert">
-                        {{ session('success') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+                <div class="card rounded border-0 shadow">
+                    @if (session()->has('success'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ session('success') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
 
-                @if (session()->has('loginerror'))
-                    <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                        {{ session('loginerror') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
-                <div class="card h-100 border-0 shadow">
-                    <div class="container m-auto my-3 mt-4">
-                        <form action="/login" method="POST">
-                            @csrf
-                            <div class="mb-2">
-                                <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" name="email" id="password" class="form-control" value="{{ old('email') }}" autofocus required>
-                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                            </div>
-                            <div class="mb-2">
-                                <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" name="password" id="password" class="form-control" id="password" required>
-                            </div>
-                            <div class="mb-1">
-                                <p>Belum memiliki akun? <a href="/register">Daftar disini!</a>
-                                </p>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Login</button>
-                        </form>
+                    @if (session()->has('loginerror'))
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            {{ session('loginerror') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
+                    <div class="card h-100 border-0 shadow">
+                        <div class="container m-auto my-3 mt-4">
+                            <form action="/login" method="POST">
+                                @csrf
+                                <div class="mb-2">
+                                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                    <input type="email" name="email" id="password" class="form-control"
+                                        value="{{ old('email') }}" autofocus required>
+                                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.
+                                    </div>
+                                </div>
+                                <div class="mb-2">
+                                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                                    <input type="password" name="password" id="password" class="form-control"
+                                        id="password" required>
+                                </div>
+                                <div class="mb-1">
+                                    <p>Belum memiliki akun? <a href="/register">Daftar disini!</a>
+                                    </p>
+                                </div>
+                                <button type="submit" class="btn btn-primary">Login</button>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,81 +1,85 @@
 @extends('Layouts.main')
 
-@section('content')
+@section('header')
     <div class="container text-center">
         <h3>Daftar Akun</h3>
         <h5>Kaderisasi Birohmah Unila</h5>
     </div>
     <div class="container mt-5">
         <div class="row row-cols-auto">
-            <div class="container col-auto col-4">
+            <div class="container col-sm-5">
                 <div class="card rounded border-0 shadow">
-                    <div class="mx-2 my-2 mt-4">
-                        <form action="/register" method="POST" class="row g-1">
-                            @csrf
-                            <input type="hidden" class="form-control " name="role" value="2">
-                            <div class="col-">
-                                <label for="inputEmail4" class="form-label">Nama</label>
-                                <input type="text" class="form-control @error('nama') is-invalid @enderror"
-                                    name="nama" id="Nama" placeholder="Nama" required value="{{ old('nama') }}">
-                                @error('nama')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="col-12">
-                                <label for="inputAddress" class="form-label">Email</label>
-                                <input type="email" class="form-control @error('email') is-invalid @enderror"
-                                    name="email" id="email" placeholder="Email" required value="{{ old('email') }}">
-                                @error('email')
-                                    <div class="invalid-feedback">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
-                            </div>
-                            <div class="col-12">
-                                <label for="inputAddress2" class="form-label">Password</label>
-                                <div class="input-group" id="password" name="password">
-                                    <input class="form-control @error('password') is-invalid @enderror" id="password"
-                                        name="password" type="password" placeholder="Password" required>
-                                    @error('password')
+                    <div class="card h-100 border-0 shadow">
+                        <div class="container m-auto my-3 mt-4">
+                            <form action="/register" method="POST" class="row g-1">
+                                @csrf
+                                <input type="hidden" class="form-control " name="role" value="2">
+                                <div class="col-">
+                                    <label for="inputEmail4" class="form-label">Nama</label>
+                                    <input type="text" class="form-control @error('nama') is-invalid @enderror"
+                                        name="nama" id="Nama" placeholder="Nama" required
+                                        value="{{ old('nama') }}">
+                                    @error('nama')
                                         <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
                                 </div>
-                            </div>
-                            <div class="col-12">
-                                <label for="inputAddress2" class="form-label">Confirm Password</label>
-                                <div class="input-group" id="confirm" name="confirm">
-                                    <input class="form-control @error('confirm') is-invalid @enderror" id="confirm"
-                                        name="confirm" type="password" placeholder="Confirm Password" required>
-                                    @error('confirm')
+                                <div class="col-12">
+                                    <label for="inputAddress" class="form-label">Email</label>
+                                    <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                        name="email" id="email" placeholder="Email" required
+                                        value="{{ old('email') }}">
+                                    @error('email')
                                         <div class="invalid-feedback">
                                             {{ $message }}
                                         </div>
                                     @enderror
                                 </div>
-                            </div>
-                            <div class="col-12 mt-3">
-                                <div class="col-md-6 captcha">
-                                    <span>{!! captcha_img() !!}</span>
+                                <div class="col-12">
+                                    <label for="inputAddress2" class="form-label">Password</label>
+                                    <div class="input-group" id="password" name="password">
+                                        <input class="form-control @error('password') is-invalid @enderror" id="password"
+                                            name="password" type="password" placeholder="Password" required>
+                                        @error('password')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-                                <div class="col-md-6 mt-2">
-                                    <input id="captcha" type="text" class="form-control @error('captcha') is-invalid @enderror" placeholder="Enter Captcha"
-                                        name="captcha" required>
+                                <div class="col-12">
+                                    <label for="inputAddress2" class="form-label">Confirm Password</label>
+                                    <div class="input-group" id="confirm" name="confirm">
+                                        <input class="form-control @error('confirm') is-invalid @enderror" id="confirm"
+                                            name="confirm" type="password" placeholder="Confirm Password" required>
+                                        @error('confirm')
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-12 mt-3">
+                                    <div class="col-md-6 captcha">
+                                        <span>{!! captcha_img() !!}</span>
+                                    </div>
+                                    <div class="col-md-6 mt-2">
+                                        <input id="captcha" type="text"
+                                            class="form-control @error('captcha') is-invalid @enderror"
+                                            placeholder="Enter Captcha" name="captcha" required>
                                         @error('captcha')
-                                        <div class="invalid-feedback">
-                                            {{ $message }}
-                                        </div>
-                                    @enderror
+                                            <div class="invalid-feedback">
+                                                {{ $message }}
+                                            </div>
+                                        @enderror
+                                    </div>
                                 </div>
-
-                            </div>
-                            <div class="col-12 mt-3 ">
-                                <button type="submit" class="btn btn-primary align-items-end ">Daftar</button>
-                            </div>
-                        </form>
+                                <div class="col-12 mt-3 ">
+                                    <button type="submit" class="btn btn-primary align-items-end ">Daftar</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
